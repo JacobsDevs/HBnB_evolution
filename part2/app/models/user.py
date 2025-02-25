c1a0498bae9db6bc8f389d15dee4a2e463c403bd
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-25 12:22:47 +1100

    add: Tests

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -6,5 +6,20 @@ class User(BaseModel):
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        self.password = password
+        if self.validate_password(password):
+            self.password = password
+        else:
+            raise Exception
         self.is_admin = is_admin
+        self.places = []
+
+    def add_place(self, place):
+        self.places.append(place)
+
+    def validate_password(self, password):
+        if password == "weak":
+            raise Exception
+
+"""    def add_review_to_place(self, place, review):
+        self.reviews.append(review)
+        place.add_review(review) """
