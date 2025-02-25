b2c99d0fcdcfadf2605469ea874e4a09d7338106
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-25 14:32:15 +1100

    add: More User tests & pytest.fixtures, refactor: User

--- part2/app/models/user.py
+++ part2/app/models/user.py
@@ -1,24 +1,77 @@
 from app.models.baseModel import BaseModel
 
 class User(BaseModel):
-    def __init__(self, first_name, last_name, email, password, is_admin): 
-        super().__init__()
+    def __init__(self, first_name=None, last_name=None, email=None, password=None, is_admin=False): 
         self.first_name = first_name
         self.last_name = last_name
         self.email = email
-        if self.validate_password(password):
-            self.password = password
-        else:
-            raise Exception
+        self.password = password
         self.is_admin = is_admin
         self.places = []
+        super().__init__()
+
+    @property
+    def first_name(self):
+        return self.__first_name
+
+    @first_name.setter
+    def first_name(self, value):
+        if value == None:
+            raise ValueError("First name is required")
+        elif self.validate_string_length(value, 50) is False:
+            raise ValueError("First name must be less than 50 characters")
+        else:
+            self.__first_name = value
+
+    @property
+    def last_name(self):
+        return self.__last_name
+
+    @last_name.setter
+    def last_name(self, value):
+        if value == None:
+            raise ValueError("Last name is required")
+        elif self.validate_string_length(value, 50) is False:
+            raise ValueError("Last name must be less than 50 characters")
+        else:
+            self.__last_name = value
+
+    @property
+    def password(self):
+        return self.__password
+
+    @password.setter
+    def password(self, value):
+        if self.validate_password(value):
+            self.__password = value
+        else:
+            raise ValueError("Password is too weak")
+
+    @property
+    def email(self):
+        return self.__email
+
+    @email.setter
+    def email(self, value):
+        if value == None:
+            raise ValueError("Email is required")
+        elif self.validate_email(value) is False:
+            raise ValueError("Email is not valid")
+        else:
+            self.__email = value
 
     def add_place(self, place):
         self.places.append(place)
 
+    def validate_string_length(self, string, length):
+        return len(string) <= length
+
     def validate_password(self, password):
-        if password == "weak":
-            raise Exception
+        return password != "weak"
+
+    def validate_email(self, email):
+        return email != "invalid"
+
 
 """    def add_review_to_place(self, place, review):
         self.reviews.append(review)
--- part2/app/tests/models/test_user.py
+++ part2/app/tests/models/test_user.py
@@ -1,19 +1,92 @@
 import pytest
 import sys
-sys.path.append('../../../')
+sys.path.append('../../..')
 from app.models.user import User
+from app.services.facade import HBnBFacade
 
 class TestClass():
+    """Pytest fixtures can be used as below to setup things for testing.
+    Keep in mind they don't persist outside their function as far as I can tell
+    so you will need to define everything that you want to persist inside the
+    fixture.
+    """
+    @pytest.fixture
+    def valid_user(self):
+        """
+        This will always contain this valid user
+        """
+        user = User("John", "Smith", "john@smith.com", "password")
+        yield user
 
-    def testUserInstantiation(self):
-        user = User("John", "Smith", "john@smith.com", "password", True)
+    @pytest.fixture
+    def setup_facade(self, valid_user):
+        """
+        This will always contain a facade user_repo object with the valid user.
+        """
+        facade = HBnBFacade()
+        facade.user_repo.add(valid_user)
+        yield facade.user_repo
+
+    def testFixturesPersist(self, setup_facade, valid_user):
+        """
+        You pass fixtures into test like this This tests that the valid_user
+        object is stored in the facade's user_repo.
+        """
+        assert setup_facade.get_all() == [valid_user]
+        assert valid_user.id == "a"
+
+    def testUserInstantiation(self, valid_user):
+        user = valid_user
         assert user.first_name == "John"
         assert user.last_name == "Smith"
         assert user.email == "john@smith.com"
         assert user.password == "password"
-        assert user.is_admin == True
+        assert user.is_admin == False
 
-    def testUserPasswordValidation(self):
+    def testUserFirstNameMissing(self):
+        """Required"""
+        with pytest.raises(Exception) as exception:
+            user = User(last_name="Smith", email="john@smith.com", password="password", is_admin=True)
+        assert exception.type == ValueError
+        assert "First name is required" in str(exception.value)
+
+    def testUserFirstNameTooLong(self):
+        """Maximum length of 50 characters."""
+        with pytest.raises(Exception) as exception:
+            user = User("J" * 51, "Smith", "john@smith.com", "password", True)
+        assert exception.type == ValueError
+        assert "First name must be less than 50 characters" in str(exception.value)
+
+    def testUserLastNameMissing(self):
+        """Required"""
+        with pytest.raises(Exception) as exception:
+            user = User(first_name="John", email="john@smith.com", password="password", is_admin=True)
+        assert exception.type == ValueError
+        assert "Last name is required" in str(exception.value)
+
+    def testUserLastNameTooLong(self):
+        """Required, maximum length of 50 characters."""
+        with pytest.raises(Exception) as exception:
+            user = User("John", "S" * 51, "john@smith.com", "password", True)
+        assert exception.type == ValueError
+        assert "Last name must be less than 50 characters" in str(exception.value)
+
+    def testUserEmailMissing(self):
+        """Required"""
+        with pytest.raises(Exception) as exception:
+            user = User(first_name="John", last_name="Smith", password="password", is_admin=True)
+        assert exception.type == ValueError
+        assert "Email is required" in str(exception.value)
+
+    def testUserEmailInvalid(self):
+        """Required, maximum length of 50 characters."""
+        with pytest.raises(Exception) as exception:
+            user = User("John", "Smith", "invalid", "password", True)
+        assert exception.type == ValueError
+        assert "Email is not valid" in str(exception.value)
+
+    def testUserPasswordWeak(self):
         with pytest.raises(Exception) as exception:
             user = User("John", "Smith", "john@smith.com", "weak", True)
-
+        assert exception.type == ValueError
+        assert "Password is too weak" in str(exception.value)
