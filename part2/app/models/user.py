a86586b6ecdfa7d1338a723d0311912b52a5c0c1
JacobsDevs <jacobdevsmail@gmail.com>
2025-02-25 11:15:20 +1100

    add: all models and repository basics

---
+++ part2/app/models/user.py
@@ -0,0 +1,10 @@
+from app.models.baseModel import BaseModel
+
+class User(BaseModel):
+    def __init__(self, first_name, last_name, email, password, is_admin): 
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.password = password
+        self.is_admin = is_admin
