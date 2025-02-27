c694a5d8f2203fcdff48306fb70de9c21efc740e
Okky <okky.mstafford@gmail.com>
2025-02-27 21:49:58 +1100

    api/amenities.py from, import, Namespace & 'amenity model' created

---
+++ part2/app/api/v1/amenities.py
@@ -0,0 +1,10 @@
+from flask_restx import Namespace, Resource, fields
+from ...services.facade import facade
+
+api = Namespace('amenities', description='Amenity Operations')
+
+# Define the amenity model(structure) for input validation and documentation
+amenity_model = api.model('Amenity', {
+    'name': fields.String(require=True, description='Name of the amenity')
+    'description': fields.String(required=True, description='Description of the amenity')
+})
