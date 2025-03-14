56e96540a2f76e223d1de381a5ed0635c03a3aca
Okky <okky.mstafford@gmail.com>
2025-03-14 11:44:25 +1100

    Added falsk-bcrypt into requirements and __init__.py file

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -1,29 +1,25 @@
 from flask import Flask
 from flask_restx import Api
 from datetime import datetime
+from flask_bcrypt import Bcrypt
 from app.services.facade import facade
 from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as places_ns
-from app.config import config as app_config
 
+# Create Bcrypt instance
+bcrypt = Bcrypt()
 
-# """This class is to convert the DateTime object into a string,
-# because Flask's JSON Serialization does not automatically handle
-# datetiem objects"""
-
-# class CustomJSONEncoder(json.JSONEncoder):
-#     def default(self, obj):
-#         if isinstance(obj, datetime):
-#             "DateTime to string"
-#             return obj.isoformat()
-#         return super().default(obj)
-
-def create_app():
+def create_app(config_class="app.config.DevelopmentConfig"):
     app = Flask(__name__)
-    # Allows us to have different configurations for development, testing and production
-    app.config.from_object(app_config[app_config])
+
+    # Apply configuration to the app
+    app.config.from_object(config_class)
+
+    # Initialize Bcrypt with the app
+    bcrypt.init_app(app)
+
     api = Api(app, version=1.0, title='HBnB aPI', description='HBnb Application API')
 
     #register the users namespace
@@ -31,5 +27,5 @@ def create_app():
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(places_ns, path='/api/v1/places')
-    
+
     return app
