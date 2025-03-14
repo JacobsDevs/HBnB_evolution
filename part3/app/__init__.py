1ad9cd3fd2e1ffe5c66f9093c4bb94f55b718221
Okky <okky.mstafford@gmail.com>
2025-03-14 11:04:20 +1100

    Added config command line. Note: Allows differnet configurations to pass for development and testing.

--- part3/app/__init__.py
+++ part3/app/__init__.py
@@ -6,6 +6,8 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as places_ns
+from app.config import config as app_config
+
 
 # """This class is to convert the DateTime object into a string,
 # because Flask's JSON Serialization does not automatically handle
@@ -20,6 +22,8 @@ from app.api.v1.places import api as places_ns
 
 def create_app():
     app = Flask(__name__)
+    # Allows us to have different configurations for development, testing and production
+    app.config.from_object(app_config[app_config])
     api = Api(app, version=1.0, title='HBnB aPI', description='HBnb Application API')
 
     #register the users namespace
@@ -27,4 +31,5 @@ def create_app():
     api.add_namespace(amenities_ns, path='/api/v1/amenities')
     api.add_namespace(reviews_ns, path='/api/v1/reviews')
     api.add_namespace(places_ns, path='/api/v1/places')
+    
     return app
