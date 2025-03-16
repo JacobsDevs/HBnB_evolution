8719e7bc87927c9262d6956650270ffaf4765aa1
Okky <okky.mstafford@gmail.com>
2025-03-17 09:09:56 +1100

    sqlalchemy_models.py > added Amenity db model table

--- part3/app/models/sqlalchemy_models.py
+++ part3/app/models/sqlalchemy_models.py
@@ -42,8 +42,15 @@ class Place(db.Model):
     # Relationships
     # Amenity > place
     # Reviews > place
-    
-class Amenity(db.Model):
 
+class Amenity(db.Model):
+    __tablename__ = 'amenities'
+        
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))
+    name = db.Column(db.String(50), nullable=False)
+    description = db.Column(db.Text, nullable=True)
+    created_at = db.Column(db.DateTime, default=func.now())
+    updated_at = db.Column(db.DateTime, default=func.now(), onupdate=func.now())
+    
 class Review(db.Model):
 
