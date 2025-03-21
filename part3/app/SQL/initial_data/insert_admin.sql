088f8d92fdc3719e7ba3b36e29ab8229c3c3649d
DSTV <9871@holbertonstudents.com>
2025-03-21 15:37:28 +1100

    SQL Script for other tables

--- part3/app/SQL/initial_data/insert_admin.sql
+++ part3/app/SQL/initial_data/insert_admin.sql
@@ -1,3 +1,4 @@
+-- Inserting into users table
 INSERT INTO users (
   id,
   email,
@@ -12,3 +13,10 @@ VALUES (
   'HBnB',
   '$2y$10$Xrilmf7skgLetD65nhVAzei./WSWfOM4AB9.AeBKm.Wiqg3goOy7.',
   1);
+
+-- Inserting into amenities table
+  INSERT INTO amenities (id, name) VALUES
+  ('bbb533ac-e2b4-4599-b5fd-5eaa0ff085e5', 'WiFi'),
+  ('8a3e5f01-4ef5-4fe6-8815-9ac58f9ae0d7', 'Swimming Pool'),
+  ('47fabf38-8594-4220-979b-88f5a365f809', 'Air Conditioning');
+  
