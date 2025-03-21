6ccbac3ae0a51fcd876712153c08b7f6d1eaf129
DSTV <9871@holbertonstudents.com>
2025-03-21 16:04:43 +1100

    task9 completed,missing some CRUD operations testing

--- part3/app/SQL/tables/create_places.sql
+++ part3/app/SQL/tables/create_places.sql
@@ -6,8 +6,8 @@ CREATE TABLE IF NOT EXISTS places (
     latitude FLOAT NOT NULL,
     longitude FLOAT NOT NULL,
     owner_id CHAR(36) NOT NULL,
-    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE,
-);
+    FOREIGN KEY (owner_id) REFERENCES users(id) ON DELETE CASCADE
+    );
 
 -- if you put ON DELETE SET NULL, this will keep the place id and set it to null
 -- and this way the place will remained even when the user dissapears
